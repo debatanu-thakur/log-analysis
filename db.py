a09053fdae547f9856b7f7aafd1a05f794e4ba31
debatanu-thakur <debatanu@gmail.com>
2018-07-29 18:38:01 +0200

    Update and change query

--- db.py
+++ db.py
@@ -21,8 +21,8 @@ class ConnectToNewsDB:
 
     def PopularArtciles(self):
         self.cursor.execute("""SELECT a.title, count(*) as views
-        from articles a inner join log l on l.path LIKE ('%'  || a.slug || '%')
-        and l.method='GET' and l.status like ('%200%')
+        from articles a inner join log l on l.path = '/article/'  || a.slug
+        and l.method='GET' and l.status = '200 OK'
         GROUP BY title order by views desc limit 3""")
         rows = self.cursor.fetchall()
         return rows
@@ -37,8 +37,8 @@ class ConnectToNewsDB:
     def PopularAuthors(self):
         self.cursor.execute("""SELECT at.name as title,
         count(*) as Views from articles a inner join log l
-        on l.path LIKE ('%'  || a.slug || '%')
-        and l.method='GET' and l.status like ('%200%')
+        on l.path = '/article/'  || a.slug
+        and l.method='GET' and l.status = '200 OK'
         inner join authors at on at.id = a.author
         GROUP BY at.name order by Views desc;""")
         rows = self.cursor.fetchall()
@@ -46,28 +46,26 @@ class ConnectToNewsDB:
 
     def ErrorPercentage(self):
         self.cursor.execute("""SELECT to_char(time, 'FMMonth DD, YYYY') Date,
-        to_char((count(*) * 100.00) / (SELECT count(*) FROM log
-        where status not like '%200%'), 'FM99.99') as Error
-        FROM log where status not like '%200%'
-        Group by Date order by Error desc limit 1;""")
+        AVG(case when status = '200 OK' then 0 else 1 end)*100 as Error
+        FROM log group by Date order by Error desc limit 1;""")
         rows = self.cursor.fetchall()
         return rows
 
+if __name__ == '__main__':
+    my_db = ConnectToNewsDB('vagrant', 'example')
+    cur = my_db.ConnectToDB()
+    rows = my_db.PopularArtciles()
+    print("1. What are the most popular three articles of all time?")
+    my_db.DisplayArticles(rows)
+    my_db.DisplayHorBar()
 
-my_db = ConnectToNewsDB('vagrant', 'example')
-cur = my_db.ConnectToDB()
-rows = my_db.PopularArtciles()
-print("1. What are the most popular three articles of all time?")
-my_db.DisplayArticles(rows)
-my_db.DisplayHorBar()
+    rows = my_db.PopularAuthors()
+    print("2. Who are the most popular article authors of all time?")
+    my_db.DisplayArticles(rows)
+    my_db.DisplayHorBar()
 
-rows = my_db.PopularAuthors()
-print("2. Who are the most popular article authors of all time?")
-my_db.DisplayArticles(rows)
-my_db.DisplayHorBar()
-
-rows = my_db.ErrorPercentage()
-print("3. On which days did more than 1%% of requests lead to errors?")
-for row in rows:
-    print("- %s--%s errors" % (row['date'], row['error']))
-my_db.DisplayHorBar()
+    rows = my_db.ErrorPercentage()
+    print("3. On which days did more than 1%% of requests lead to errors?")
+    for row in rows:
+        print("- %s--%2.2f%% errors" % (row['date'], row['error']))
+    my_db.DisplayHorBar()
